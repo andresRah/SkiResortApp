492867424a69a1da18add131309d991edf29d6cd
Andrés Leonardo Arévalo Parra <aarevalop@intergrupo.com>
2019-02-16 15:33:06 -0500

    Splash and icons

--- SkiResort/SkiResort.Android/MainActivity.cs
+++ SkiResort/SkiResort.Android/MainActivity.cs
@@ -9,7 +9,9 @@ using Android.OS;
 
 namespace SkiResort.Droid
 {
-    [Activity(Label = "SkiResort", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "SkiResort", Icon = "@mipmap/icon", 
+              Theme = "@style/MainTheme", MainLauncher = false, 
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- SkiResort/SkiResort.Android/Resources/Resource.designer.cs
+++ SkiResort/SkiResort.Android/Resources/Resource.designer.cs
@@ -2342,26 +2342,26 @@ namespace SkiResort.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020136
-			public const int avd_hide_password_1 = 2130837814;
-			
 			// aapt resource value: 0x7f020137
-			public const int avd_hide_password_2 = 2130837815;
+			public const int avd_hide_password_1 = 2130837815;
 			
 			// aapt resource value: 0x7f020138
-			public const int avd_hide_password_3 = 2130837816;
+			public const int avd_hide_password_2 = 2130837816;
+			
+			// aapt resource value: 0x7f020139
+			public const int avd_hide_password_3 = 2130837817;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020139
-			public const int avd_show_password_1 = 2130837817;
-			
 			// aapt resource value: 0x7f02013a
-			public const int avd_show_password_2 = 2130837818;
+			public const int avd_show_password_1 = 2130837818;
 			
 			// aapt resource value: 0x7f02013b
-			public const int avd_show_password_3 = 2130837819;
+			public const int avd_show_password_2 = 2130837819;
+			
+			// aapt resource value: 0x7f02013c
+			public const int avd_show_password_3 = 2130837820;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -3002,11 +3002,11 @@ namespace SkiResort.Droid
 			// aapt resource value: 0x7f020129
 			public const int notification_icon_background = 2130837801;
 			
-			// aapt resource value: 0x7f020134
-			public const int notification_template_icon_bg = 2130837812;
-			
 			// aapt resource value: 0x7f020135
-			public const int notification_template_icon_low_bg = 2130837813;
+			public const int notification_template_icon_bg = 2130837813;
+			
+			// aapt resource value: 0x7f020136
+			public const int notification_template_icon_low_bg = 2130837814;
 			
 			// aapt resource value: 0x7f02012a
 			public const int notification_tile_bg = 2130837802;
@@ -3027,16 +3027,19 @@ namespace SkiResort.Droid
 			public const int screenTwoE = 2130837807;
 			
 			// aapt resource value: 0x7f020130
-			public const int stars = 2130837808;
+			public const int splash = 2130837808;
 			
 			// aapt resource value: 0x7f020131
-			public const int tooltip_frame_dark = 2130837809;
+			public const int stars = 2130837809;
 			
 			// aapt resource value: 0x7f020132
-			public const int tooltip_frame_light = 2130837810;
+			public const int tooltip_frame_dark = 2130837810;
 			
 			// aapt resource value: 0x7f020133
-			public const int xamarin_logo = 2130837811;
+			public const int tooltip_frame_light = 2130837811;
+			
+			// aapt resource value: 0x7f020134
+			public const int xamarin_logo = 2130837812;
 			
 			static Drawable()
 			{
@@ -4194,8 +4197,8 @@ namespace SkiResort.Droid
 			// aapt resource value: 0x7f0c016e
 			public const int Animation_Design_BottomSheetDialog = 2131493230;
 			
-			// aapt resource value: 0x7f0c0191
-			public const int AppCompatDialogStyle = 2131493265;
+			// aapt resource value: 0x7f0c0192
+			public const int AppCompatDialogStyle = 2131493266;
 			
 			// aapt resource value: 0x7f0c00a9
 			public const int Base_AlertDialog_AppCompat = 2131493033;
@@ -4698,11 +4701,11 @@ namespace SkiResort.Droid
 			// aapt resource value: 0x7f0c000e
 			public const int CardView_Light = 2131492878;
 			
-			// aapt resource value: 0x7f0c018f
-			public const int MainTheme = 2131493263;
-			
 			// aapt resource value: 0x7f0c0190
-			public const int MainTheme_Base = 2131493264;
+			public const int MainTheme = 2131493264;
+			
+			// aapt resource value: 0x7f0c0191
+			public const int MainTheme_Base = 2131493265;
 			
 			// aapt resource value: 0x7f0c002d
 			public const int Platform_AppCompat = 2131492909;
@@ -5093,6 +5096,9 @@ namespace SkiResort.Droid
 			
 			// aapt resource value: 0x7f0c0006
 			public const int Theme_MediaRouter_LightControlPanel = 2131492870;
+			
+			// aapt resource value: 0x7f0c018f
+			public const int Theme_Splash = 2131493263;
 			
 			// aapt resource value: 0x7f0c0116
 			public const int ThemeOverlay_AppCompat = 2131493142;
