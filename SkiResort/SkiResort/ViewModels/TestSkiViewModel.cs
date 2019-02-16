9f9b366de1c4fd2481a36aa6f387cdf0735406fd
Andrés Leonardo Arévalo Parra <aarevalop@intergrupo.com>
2019-02-16 00:45:50 -0500

    Link ViewModel With XAML View

---
+++ SkiResort/SkiResort/ViewModels/TestSkiViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SkiResort.ViewModels
+{
+    using System;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class TestSkiViewModel : BaseViewModel
+    {
+        public TestSkiViewModel()
+        {
+            Title = "About";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
